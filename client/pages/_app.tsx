ccfd15ca8278dfb0deb3b8723f72807dff771d57
YangGwangSeong <rhkdtjd_12@naver.com>
2023-03-06 19:25:39 -0800

    feat: header navbar ui 생성

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,20 @@
+import NavBar from '@/components/ui/layout/NavBar';
 import '@/styles/globals.css';
 import { AuthProvider } from 'context/auth';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+	const { pathname } = useRouter();
+	const authRoutes = ['/register', '/login'];
+	const authRoute = authRoutes.includes(pathname);
+
 	return (
 		<AuthProvider>
-			<Component {...pageProps} />
+			{!authRoute && <NavBar></NavBar>}
+			<div className={authRoutes ? '' : 'pt-12'}>
+				<Component {...pageProps} />
+			</div>
 		</AuthProvider>
 	);
 }
