e8bbbe4882b4d91d35cd11b909c793cfa8d9d937
YangGwangSeong <rhkdtjd_12@naver.com>
2023-03-12 10:06:11 -0800

    feat: sub 상세페이지 UI생성

--- client/pages/r/[sub].tsx
+++ client/pages/r/[sub].tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { NextPage } from 'next';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 import React from 'react';
 import useSWR from 'swr';
@@ -22,8 +23,56 @@ const SubPage: NextPage = () => {
 		subName ? `/api/subs/${subName}` : null,
 		fetcher,
 	);
-
-	return <div>subs</div>;
+	console.log(sub);
+	return (
+		<>
+			{sub && (
+				<>
+					<div>
+						<div className="bg-gray-400">
+							{sub.bannerUrl ? (
+								<div
+									className="h-56"
+									style={{
+										backgroundImage: `url(${sub.bannerUrl})`,
+										backgroundRepeat: 'no-repeat',
+										backgroundSize: 'cover',
+										backgroundPosition: 'center',
+									}}
+								></div>
+							) : (
+								<div className="h-20 bg-gray-400"></div>
+							)}
+						</div>
+						<div className="h-20 bg-white">
+							<div className="relative flex mx-w-5xl px-5 mx-auto">
+								<div className="absolute" style={{ top: -15 }}>
+									{sub.imageUrl && (
+										<Image
+											src={sub.imageUrl}
+											alt="커뮤니티 이미지"
+											width={70}
+											height={70}
+											className="rounded-full"
+										></Image>
+									)}
+								</div>
+								<div className="pt-1 pl-24">
+									<div className="flex items-center">
+										<h1 className="text-3xl font-bold">{sub.title}</h1>
+									</div>
+									<p className="text-small font-bold text-gray-400">
+										/r/{sub.name}
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="flex mx-w-5xl px-4 pt-5 mx-auto"></div>
+				</>
+			)}
+		</>
+	);
 };
 
 export default SubPage;
