62470be15b4901a6fbb7ea71a6d995b2144d8575
YangGwangSeong <rhkdtjd_12@naver.com>
2023-03-28 15:21:44 -0800

    feat: post-card UI 생성

--- client/pages/r/[sub].tsx
+++ client/pages/r/[sub].tsx
@@ -1,4 +1,6 @@
 import Sidebar from '@/components/ui/layout/Sidebar';
+import PostCard from '@/components/ui/post-card/PostCard';
+import { Post } from '@/shared/interfaces/post.interface';
 import { Sub } from '@/shared/interfaces/sub.interfeace';
 import axios from 'axios';
 import { useAuthStateDispatch } from 'context/auth';
@@ -52,6 +54,18 @@ const SubPage: NextPage = () => {
 		setOwnSub(authenticated && user?.username === sub.username);
 	}, [sub]);
 
+	let renderPosts;
+	if (!sub) {
+		renderPosts = <p className="text-lg text-center">로딩중...</p>;
+	} else if (sub.posts.length === 0) {
+		renderPosts = (
+			<p className="text-lg text-center">아직 작성된 포스트가 없습니다.</p>
+		);
+	} else {
+		renderPosts = sub.posts.map((post: Post) => (
+			<PostCard key={post.identifier} post={post} />
+		));
+	}
 	return (
 		<>
 			{sub && (
@@ -107,8 +121,9 @@ const SubPage: NextPage = () => {
 							</div>
 						</div>
 					</div>
+					{/* 포스트 & 사이드바 */}
 					<div className="flex mx-w-5xl px-4 pt-5 mx-auto">
-						<div className="w-full md:mr-3 md:w-8/12 "></div>
+						<div className="w-full md:mr-3 md:w-8/12 ">{renderPosts}</div>
 						<Sidebar sub={sub}></Sidebar>
 					</div>
 				</>
